df767e28dbec9f36842062a255ac4eedf709aad2
lifei <life@31huiyi.com>
2018-12-04 16:42:10 +0800

    提交 上线

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const static = require('koa-static');
+const app = new Koa();
+
+console.log(__dirname)
+// 配置静态web服务的中间件
+app.use(static(__dirname+'/www'));
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(8000,function(){
+    console.log('开启服务器成功')
+});
